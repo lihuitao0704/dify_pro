edb05080951d147c9308b10d67b393adf0a6bfb8
Your Name <you@example.com>
2026-07-12 20:37:30 +0800

    修改端口

--- Assessment/resume_api.py
+++ Assessment/resume_api.py
@@ -983,5 +983,5 @@ if __name__ == "__main__":
     import uvicorn
 
     host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", "8080"))
+    port = int(os.getenv("PORT", "8007"))
     uvicorn.run("Assessment.resume_api:app", host=host, port=port, reload=True)
